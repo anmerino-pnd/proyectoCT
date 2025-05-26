365652a5fa2cfec559098ceca2dc4ccf40be90d6
Angel Merino <acedeno00@gmail.com>
2025-05-26 11:14:33 -0700

    mejor diseño del ETL

--- src/ct/ETL/load.py
+++ src/ct/ETL/load.py
@@ -19,50 +19,70 @@ class Load:
         self.sales_collection = self.db[mongo_collection_sales]
 
     def build_content(self, product : dict, product_features : list):
-        return ". ".join(f"{product_feature.capitalize()}: {product.get(product_feature, 'No disponible')}" for product_feature in product_features if product.get(product_feature))
+        return ". ".join(
+            f"{product_feature.capitalize()}: {product.get(product_feature, 'No disponible')}" 
+            for product_feature in product_features 
+            if product.get(product_feature))
  
+    def mongo_products(self):
+        products = self.clean_data.clean_products()
+        for product in products:
+            clave = product.get("clave")
+            if clave:
+                self.products_collection.update_one(
+                    {"clave": clave},           # filtro: busca por clave
+                    {"$set": product},          # operación: actualiza todo el documento con los nuevos valores
+                    upsert=True                 # si no existe, lo inserta
+                )
+
+
+    def mongo_sales(self):
+        sales = self.clean_data.clean_sales()
+        for sale in sales:
+            clave = sale.get("clave")
+            if clave:
+                self.sales_collection.update_one(
+                    {"clave": clave},
+                    {"$set": sale},
+                    upsert=True
+                )
+
     def load_products(self):
-        products = self.clean_data.clean_products() 
+        products = self.products_collection.find()
         
         product = products[0]  
-        product_features = [column for column in product.keys()]
+        product_features = [column for column in product.keys() if column != "_id"]
 
         docs = [
             Document(
-                page_content=self.build_content(product, product_features)
+                page_content=self.build_content(product, product_features),
+                metadata = {"_id": str(product["_id"])}
             )
             for product in products 
         ]
         return docs
 
     def load_sales(self):
-        sales = self.clean_data.clean_sales()
+        sales = self.sales_collection.find()
         
         sale = sales[0]
-        sales_features = [column for column in sale.keys()]
+        sales_features = [column for column in sale.keys() if column != "_id"]
         docs = [
             Document(
-                page_content=self.build_content(sale, sales_features)
+                page_content=self.build_content(sale, sales_features),
+                metadata = {"_id": str(sale["_id"])}
             )
             for sale in sales
         ]
         return docs
     
-    def mongo_products(self):
-        products = self.load_products()
-        for product in products:
-            self.products_collection.insert_one(product)
-
-    def mongo_sales(self):
-        sales = self.load_sales()
-        for sale in sales:
-            self.sales_collection.insert_one(sale)
-    
     def vector_store(self, docs: Document) -> FAISS:
         vector_store = FAISS.from_documents(docs, self.embeddings)
         return vector_store
     
-    def products_vs(self, products: list):
+    def products_vs(self):
+        products = self.load_products()
+
         # --- Batch size ---
         batch_size = 250
         # Create the vector store
@@ -77,20 +97,23 @@ class Load:
         vector_store.save_local(str(PRODUCTS_VECTOR_PATH))
         print("Vector store created and saved to disk.")
 
-    def sales_products_vs(self, sales: list):
+    def sales_products_vs(self):
+        sales = self.load_sales()
+
         # --- Batch size ---
         batch_size = 200
         # Create the vector store
         total_docs = len(sales)
-        vector_store = self.vector_store(sales[:batch_size]) 
-
+        vector_store = FAISS.load_local(folder_path=str(PRODUCTS_VECTOR_PATH), 
+                                          embeddings=self.embeddings, allow_dangerous_deserialization=True)
+        vector_store.add_documents(sales[:batch_size])
         for i in range(1, total_docs, batch_size):
             batch = sales[i:i + batch_size]
             vector_store.add_documents(batch)
             print(f"Processed {i + batch_size} of {total_docs} documents.")
-
+        
         vector_store.save_local(str(SALES_PRODUCTS_VECTOR_PATH))
-        print("Merged vector store created and saved to disk.")
+        print("Sales vector store created and saved to disk.")
         
     
     
