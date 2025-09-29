83e552f11277806e7f24ac87e3812598bbb040c8
Angel Merino <acedeno00@gmail.com>
2025-09-29 11:11:36 -0700

    Update ct_info.py

--- src/ct/tools/ct_info.py
+++ src/ct/tools/ct_info.py
@@ -1,22 +1,20 @@
-
-
 def who_are_we():
     return """
 ¿Quiénes somos?
-CT Internacional es una empresa 100% mexicana y el distribuidor líder en soluciones de Tecnologías de la Información (TI) además es la empresa preferida para hacer negocios de los distribuidores e integradores de tecnología.
-Tiene presencia en todos los estados del País con 52 sucursales y cuenta con un canal de distribución integrado por más de 31 mil clientes, aliados de negocio, a quienes proporciona un extenso catálogo de productos y servicios de más de 202 marcas agrupadas en 12 unidades de negocio.
-CT da empleo a más de 1000 colaboradores.
+CT Internacional es una empresa 100% mexicana y el distribuidor líder de soluciones en Tecnologías de la Información (TI). Somos la opción preferida de distribuidores e integradores de tecnología para hacer negocios.
 
-Valores:
-* Pasión: Nos gusta nuestro trabajo, lo realizamos con entusiasmo y compromiso para lograr la lealtad de nuestros socios de negocios.
-* Trabajo en Equipo: Sumamos nuestras fortalezas para alcanzar un mismo objetivo y obtener resultados extraordinarios.
-* Honestidad: Actuamos con honradez e integridad, viviendo los ideales de CT.
-* Innovación: Somos creativos y nos reinventamos constantemente para ofrecer más valor.
-* Servicio al Cliente: El cliente es primero, trabajamos todos los días para superar sus expectativas.
-* Responsabilidad: Estamos comprometidos a generar un desarrollo sustentable para nuestra sociedad.
-* Calidad: Hacemos las cosas bien desde la primera vez para lograr resultados que nos distingan.
+Contamos con presencia en todos los estados del país a través de 52 sucursales y un canal de distribución conformado por más de 31,000 clientes y aliados estratégicos. Les ofrecemos un amplio catálogo de productos y servicios de más de 202 marcas agrupadas en 12 unidades de negocio. Además, generamos empleo para más de 1,000 colaboradores.
 
-Misión: Elevar la competitividad y productividad de las personas y organizaciones para alcanzar su máximo potencial mediante el uso de la tecnología.
+Nuestros valores:
+* **Pasión:** Amamos lo que hacemos y trabajamos con entusiasmo y compromiso para ganar la lealtad de nuestros socios de negocio.
+* **Trabajo en equipo:** Sumamos fortalezas para alcanzar objetivos comunes y lograr resultados extraordinarios.
+* **Honestidad:** Actuamos con honradez e integridad, viviendo los ideales de CT.
+* **Innovación:** Somos creativos y nos reinventamos constantemente para ofrecer más valor.
+* **Servicio al cliente:** El cliente es primero; trabajamos cada día para superar sus expectativas.
+* **Responsabilidad:** Estamos comprometidos con el desarrollo sustentable de nuestra sociedad.
+* **Calidad:** Hacemos las cosas bien desde la primera vez para obtener resultados que nos distingan.
 
-Visión: Ser la empresa líder que proporcione soluciones integrales de tecnología a todas las personas y organizaciones de México.
+**Misión:** Elevar la competitividad y productividad de las personas y organizaciones para que alcancen su máximo potencial mediante el uso de la tecnología.
+
+**Visión:** Ser la empresa líder en México que proporcione soluciones integrales de tecnología a todas las personas y organizaciones.
 """
