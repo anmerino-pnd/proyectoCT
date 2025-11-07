17c8ae14fc5482fd01d1ed859ff00df6b883dc4a
Angel Merino <acedeno00@gmail.com>
2025-11-07 09:02:39 -0700

    Update cache.py

--- src/ct/settings/cache.py
+++ src/ct/settings/cache.py
@@ -1,9 +1,16 @@
 import redis
-import cassio
-from langchain_community.cache import RedisCache, RedisSemanticCache, InMemoryCache, CassandraSemanticCache
-from ct.settings.clients import podman_redis_url, openai_api_key
+import gptcache
+from langchain_community.cache import (
+    GPTCache,
+    RedisCache, 
+    InMemoryCache, 
+    RedisSemanticCache, 
+    CassandraSemanticCache,)
 from langchain.globals import set_llm_cache
+from gptcache.processor.pre import get_prompt
 from langchain_openai import OpenAIEmbeddings
+from ct.settings.clients import podman_redis_url, openai_api_key
+from gptcache.manager.factory import get_data_manager, CacheBase, VectorBase
 
 # # Conectar a Cassandra en Podman
 # cassio.init(
@@ -24,8 +31,17 @@ from langchain_openai import OpenAIEmbeddings
 # cache_client = InMemoryCache()
 # set_llm_cache(cache_client)
 
-redis_client = redis.Redis.from_url(podman_redis_url)
-set_llm_cache(RedisCache(redis_client, ttl=600))
+def init_gptcache(cache_obj: gptcache.Cache):
+    cache_obj.init(
+        pre_embedding_func=get_prompt,
+        data_manager=get_data_manager(CacheBase('sqlite'), VectorBase('faiss', dimension=128))
+        )
+
+set_llm_cache(GPTCache(init_gptcache))
+
+
+# redis_client = redis.Redis.from_url(podman_redis_url)
+# set_llm_cache(RedisCache(redis_client, ttl=600))
 
 # redis_client = RedisSemanticCache(                                        # Error de pip install que no se arregló
 #                 redis_url=podman_redis_url,
