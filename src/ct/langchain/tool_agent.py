b3b511e1a387f786d179d817064c6ae2bc090452
Angel Merino <acedeno00@gmail.com>
2025-11-24 10:10:58 -0700

    Update tool_agent.py

--- src/ct/langchain/tool_agent.py
+++ src/ct/langchain/tool_agent.py
@@ -99,7 +99,7 @@ class ToolAgent:
             StructuredTool.from_function(
                 func=get_support_info,
                 name="get_support_info",
-                description="Cuando necesites saber sobre cómo hacer compras en líneas, compras y envíos de ESD, políticas, garantías, devoluciones, términos y condiciones, partnerCT (ventas e-commerce)",
+                description="Cuando necesites saber sobre cómo hacer compras en líneas, compras y envíos de ESD, políticas, garantías, devoluciones, términos y condiciones, partnerCT (ventas e-commerce), Directorio de Product Managers",
                 args_schema=SupportInput
         ),
             StructuredTool.from_function(
