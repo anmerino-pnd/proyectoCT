2bbeedc05a0f43eec0a2bb80491f14a68677a71b
Angel Merino <acedeno00@gmail.com>
2025-12-16 11:06:36 -0700

    Update tool_agent.py

--- src/ct/langchain/tool_agent.py
+++ src/ct/langchain/tool_agent.py
@@ -41,7 +41,7 @@ timing_callback = TimingCallbackHandler()
 
 class ToolAgent:
     def __init__(self):
-        self.model = "gpt-5-nano"
+        self.model = "gpt-4.1-mini"
         print("Cache actual:", get_llm_cache())
         
         self.rate_limiter = InMemoryRateLimiter(
